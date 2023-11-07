df7251523041a58e59c3575f3a69aa0a8645543f
Ali Aminzadeh <ali.aminzadeh@tapsi.cab>
2023-11-07 19:34:17 +0330

    2nd comit

--- examples.scala
+++ examples.scala
@@ -1,22 +1,11 @@
-def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+def findFirst[A](as: Array[A], p: A => Boolean): Int = {
   @annotation.tailrec
-  def go(n: Int): Boolean = {
-    if (n >= (as.length-1))  true
-    else if (gt(as(n), as(n+1)))  false
-    else go(n+1)
+  def loop(n: Int): Int = {
+    if (n >= as.length) -1
+    else if (p(as(n))) n
+    else loop(n + 1)
   }
-  go(0)
+  loop(0)
 }
 
-val array1 = Array(1, 2, 3, 4, 5)
-val array2 = Array("apple", "banana", "cherry")
-val array3 = Array(5.6, 4.3, 2.1)
-
-def greaterThan(a: Int, b: Int): Boolean = a > b
-
-val sorted1 = isSorted(array1, greaterThan)
-// val sorted2 = isSorted(array2, greaterThan)
-// val sorted3 = isSorted(array3, greaterThan)
-
-
-println(sorted1)
+println(findFirst(Array(7, 9, 13), (x: Int) => x == 9))
